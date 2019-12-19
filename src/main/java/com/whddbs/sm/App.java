f50ceb58e30515f164bd3bb05e7d814fbdb6714d
whddbs507 <whddbs5071@naver.com>
2019-12-19 18:46:11 +0900

    1. 프로젝트 구성 및 코드 입력

--- src/main/java/com/whddbs/sm/App.java
+++ src/main/java/com/whddbs/sm/App.java
@@ -1,7 +1,54 @@
 package com.whddbs.sm;
 
+import java.util.Scanner;
+import java.sql.Date;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("스케쥴 관리@@@@");
+
+    Scanner keyScan = new Scanner(System.in);
+
+    class Contents {
+      int no;
+      String name;
+      String description;
+      Date startDay;
+      Date endDay;
+      int totalHour;
+      int dayHour;      
+    }
+    final int SIZE = 100;
+    Contents[] contentsArray = new Contents[SIZE];
+    int count = 1;
+
+    for (int i = 0; i < count; i++) {
+      Contents aa = new Contents();
+      contentsArray[i] = aa;
+      aa.no = count;
+      System.out.print("수업명 ?  ");
+      aa.name = keyScan.nextLine();
+      System.out.print("설명 ?  ");
+      aa.description = keyScan.nextLine();
+      System.out.print("시작일 ?  ");
+      aa.startDay = Date.valueOf(keyScan.nextLine());
+      System.out.print("종료일 ?  ");
+      aa.endDay = Date.valueOf(keyScan.nextLine());
+      System.out.print("총수업시간 ?  ");
+      aa.totalHour = keyScan.nextInt();
+      keyScan.nextLine();
+      System.out.print("일수업시간 ?  ");
+      aa.dayHour = keyScan.nextInt();
+      keyScan.nextLine();
+      System.out.println("계속 입력하시겠습니까? (Y/n)");
+      String response = keyScan.nextLine();
+      if (!response.equalsIgnoreCase("y")) {
+        break;
+      }
+      count++;
+    }
+    for (int i = 0; i < count; i++) {
+      Contents bb = contentsArray[i];
+      System.out.printf("%d, %s, %s, %s, %s, %d, %d\n", bb.no, bb.name, bb.description, bb.startDay, bb.endDay, bb.totalHour, bb.dayHour);  
+    }
   }
 }
